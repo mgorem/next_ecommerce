ceff099f8c74fc9ee1c33dc30c4e2cac13f12b96
/omg <oremgitonga22@gmail.com>
2023-04-15 16:55:41 +0300

    dashboard setup and routing on point

--- admin_app/pages/index.js
+++ admin_app/pages/index.js
@@ -1,21 +1,21 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 px-4 rounded-lg"
-          >
-            Login With Google
-          </button>
+  if (!session) return;
+
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Welcome back, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt="" className="w-6 h-6" />
+          <span className="px-2">{session?.user?.name}</span>
         </div>
       </div>
-    );
-  }
-
-  return <div>Logged in as {session.user.email}</div>;
+    </Layout>
+  );
 }
